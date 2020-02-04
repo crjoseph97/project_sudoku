3b1e2b71ba331c238b2991c43a63e5b5b0d0d34a
Joseph Chungath <josephc@hifx.co.in>
2020-02-04 14:56:00 +0530

    error checking

--- src/table.jsx
+++ src/table.jsx
@@ -1,37 +1,58 @@
 import React from 'react';
+import classNames from 'classnames';
 import './table.css';
 
+//---------------------------------------------------------------------------------
+//Square signifies each Cell in the Sudoku Table
 class Square extends React.Component {
-
   render() {
       return (
-        <input type="text" className="square" onChange={(e)=> this.props.handleChange(e.target.value,this.props.boxValue)}/>
+          <input type="text" className={this.props.getClassName(this.props.boxValue)}    onChange={(e)=> this.props.handleChange(e.target.value,this.props.boxValue)}/>
       );
-    }
   }
-  
-  class Box extends React.Component {
+}
+//---------------------------------------------------------------------------------  
+//Box component represents each box in the table
+class Box extends React.Component {
     constructor() {
       super()
       this.state = {
-         boxArray : new Array(9).fill(0)
+         boxArray : new Array(9).fill(0),
+         inpErr : new Array(9).fill(false)
       }
       this.handleChange=this.handleChange.bind(this)
+      this.getClassName=this.getClassName.bind(this)
     }
     renderSquare(i) {
         return  (
-           <Square boxValue={i} handleChange={this.handleChange}/>
+           <Square boxValue={i} handleChange={this.handleChange} getClassName={this.getClassName}/>
 
         )
-      }
+    }
+    getClassName(i) {
+      if (this.state.inpErr[i] === false)
+        return classNames('square')
+      else
+        return classNames("errorSquare")
+    }
     handleChange(input,value) {
+      if (!isNaN(input) && input<10 && input>=0) {
+        let newArr = this.state.boxArray.slice(0)
+        newArr[value] = input
+        this.setState({boxArray : newArr},()=>{this.props.updateBoardArray(this.state.boxArray)})
+        let errArr = this.state.inpErr.slice(0)
+        errArr[value] = false
+        this.setState({ inpErr : errArr})
 
-      // console.log(value)
-      let newArr = this.state.boxArray.slice(0)
-      newArr[value] = input
-      // console.log(newArr)
-      this.setState({boxArray : newArr},()=>{this.props.updateBoardArray(this.state.boxArray)})
-
+      } 
+      else {       
+        // alert("Invalid input")
+        console.log("Invalid Input")
+        let errArr = this.state.inpErr.slice(0)
+        errArr[value] = true
+        this.setState({ inpErr : errArr})
+        
+      }
     }  
     render() {
         return (
@@ -54,76 +75,77 @@ class Square extends React.Component {
           </div>
         )
       }
-  }
-  class Board extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        boardArray : new Array(9).fill(0)
-
-     }
-      this.updateBoardArray=this.updateBoardArray.bind(this)
     }
 
-    renderBox(i) {
-      return <Box boardValue={i} updateBoardArray={(arr)=>{this.updateBoardArray(arr,i)}}  />;
-    }
-    updateBoardArray(arr, i) {
-      let newArr = this.state.boardArray.slice(0)
-      newArr[i] = arr
-      console.log(newArr)
-      this.setState( {
-        boardArray : newArr
-            })
-    }
+//------------------------------------------------------------------------------------------    
+//Board is used to represent the entire SUdoku table
+class Board extends React.Component {
+      constructor(props) {
+        super(props)
+        this.state = {
+          boardArray : new Array(9).fill(0)
+        }
+        this.updateBoardArray=this.updateBoardArray.bind(this)
+      }
+      
+      renderBox(i) {
+        return <Box boardValue={i} updateBoardArray={(arr)=>{this.updateBoardArray(arr,i)}}  />;
+      }
+      updateBoardArray(arr, i) {
+        let newArr = this.state.boardArray.slice(0)
+        newArr[i] = arr
+        console.log(newArr)
+        this.setState( {
+          boardArray : newArr
+        })
+      } 
    
-    render() {
-     //const status = 'Next player: X';
+      render() {
   
-      return (
-        <div className="board">
-          <div className="board-row">
-            {this.renderBox(0)}
-            {this.renderBox(1)}
-            {this.renderBox(2)}
+        return (
+          <div className="board">
+            <div className="board-row">
+              {this.renderBox(0)}
+              {this.renderBox(1)}
+              {this.renderBox(2)}
+            </div>
+            <div className="board-row">
+              {this.renderBox(3)}
+              {this.renderBox(4)}
+              {this.renderBox(5)}
+            </div>
+            <div className="board-row">
+              {this.renderBox(6)}
+              {this.renderBox(7)}
+              {this.renderBox(8)}
+            </div>
           </div>
-          <div className="board-row">
-            {this.renderBox(3)}
-            {this.renderBox(4)}
-            {this.renderBox(5)}
-          </div>
-          <div className="board-row">
-            {this.renderBox(6)}
-            {this.renderBox(7)}
-            {this.renderBox(8)}
-          </div>
-        </div>
-      );
+        );
+      }
     }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="header">
-            <h1>Sudoku!</h1>
-          </div>
+    
+class Game extends React.Component {
+      render() {
+        return (
+          <div className="game">
+            <div className="header">
+             <h1>Sudoku!</h1>
+            </div>
          
-          <div className="game-board">
-            <Board />
+            <div className="game-board">
+              <Board />
+            </div>
+            <div className="game-info">
+              <div>{/* status */}</div>
+              <ol>{/* TODO */}</ol>
+            </div>
+            <div className="footer">
+            </div>
           </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-          <div className="footer">
-          </div>
-        </div>
-      );
+        );
+      }
     }
-  }
-  export default Game;
-  // ========================================
+export default Game;
+// ========================================
   
  
